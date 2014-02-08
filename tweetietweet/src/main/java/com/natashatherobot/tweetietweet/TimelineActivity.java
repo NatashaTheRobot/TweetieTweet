b40ca4ca525bc6e4158132a94b47a725518755fb
Natasha Murashev <nmurashev@gmail.com>
2014-02-08 10:03:05 -0800

    added fragments

--- tweetietweet/src/main/java/com/natashatherobot/tweetietweet/TimelineActivity.java
+++ tweetietweet/src/main/java/com/natashatherobot/tweetietweet/TimelineActivity.java
@@ -1,6 +1,8 @@
 package com.natashatherobot.tweetietweet;
 
 import android.annotation.TargetApi;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -18,16 +20,23 @@ import org.json.JSONArray;
 
 import java.util.ArrayList;
 
+import fragments.HomeTimelineFragment;
+import fragments.MentionsFragment;
 import fragments.TweetsListFragment;
 import helpers.TweetieBirdApp;
 import models.Tweet;
 
-public class TimelineActivity extends ActionBarActivity {
+public class TimelineActivity extends ActionBarActivity implements ActionBar.TabListener {
+
+    private HomeTimelineFragment homeTimelineFragment;
+    private MentionsFragment mentionsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
+
+        setupNavigationTabs();
     }
 
 
@@ -49,5 +58,53 @@ public class TimelineActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupNavigationTabs() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowTitleEnabled(true);
+
+        ActionBar.Tab tabHome = actionBar.newTab().setText("Home")
+                                                  .setTag("HomeTimelineFragment")
+                                                  .setIcon(R.drawable.ic_action_timeline)
+                                                  .setTabListener(this);
+
+        ActionBar.Tab tabMentions = actionBar.newTab().setText("Mentions")
+                                                      .setTag("MentionsFragment")
+                                                      .setIcon(R.drawable.ic_action_mentions)
+                                                      .setTabListener(this);
+
+        actionBar.addTab(tabHome);
+        actionBar.addTab(tabMentions);
+        actionBar.selectTab(tabHome);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        FragmentManager manager = getSupportFragmentManager();
+        fragmentTransaction = manager.beginTransaction();
+        if (tab.getTag() == "HomeTimelineFragment") {
+            if (homeTimelineFragment == null) {
+                homeTimelineFragment = new HomeTimelineFragment();
+            }
+            fragmentTransaction.replace(R.id.frameContainer, homeTimelineFragment);
+        } else {
+            if (mentionsFragment == null) {
+                mentionsFragment = new MentionsFragment();
+            }
+            fragmentTransaction.replace(R.id.frameContainer, mentionsFragment);
+        }
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
     }
 }
