4e07c5a1ffe72fe2acf881ecf0d0ad0b7a379bee
Natasha Murashev <nmurashev@gmail.com>
2014-02-08 06:50:06 -0800

    configured login view

--- tweetietweet/src/main/java/com/natashatherobot/tweetietweet/LoginActivity.java
+++ tweetietweet/src/main/java/com/natashatherobot/tweetietweet/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.natashatherobot.tweetietweet;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
@@ -19,31 +20,14 @@ import helpers.TwitterClient;
 
 public class LoginActivity extends OAuthLoginActivity<TwitterClient> {
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        getActionBar().hide();
     }
 
     // OAuth authenticated successfully, launch primary authenticated activity
@@ -66,7 +50,7 @@ public class LoginActivity extends OAuthLoginActivity<TwitterClient> {
     // Click handler method for the button used to start OAuth flow
     // Uses the client to initiate OAuth authorization
     // This should be tied to a button used to login
-    public void loginToRest(View view) {
+    public void loginToTwitter(View view) {
         getClient().connect();
     }
 
